49f0570e4d1331ae7c0d75ee168e38f1af376287
Dave Glover <dglover@microsoft.com>
2019-08-07 15:08:56 +1000

    doco - update to flask app

--- Lab1-ssh-debug/flask-main.py
+++ Lab1-ssh-debug/flask-main.py
@@ -14,14 +14,21 @@ bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
 
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
 
+
 @app.route('/')
 @app.route('/index')
-def hello_world():
-    return render_template('index.html', title='Raspberry Pi Environment Data',
-                           temperature=round(bme280.temperature, 1), pressure=round(bme280.pressure),
-                           humidity=round(bme280.humidity))
+def telemetry():
+
+    title = "Raspberry Pi Environment Data"
+    temperature = round(bme280.temperature, 1)
+    pressure = round(bme280.pressure)
+    humidity = round(bme280.humidity)
+
+    return render_template('index.html', title=title,
+                           temperature=temperature, pressure=pressure,
+                           humidity=humidity)
 
 
 if __name__ == '__main__':
     # Run the server
-    app.run(host='<Raspberry PI IP Address>', port=port)
+    app.run(host='10.168.10.59', port=port)
