b8bc3965ce7d6deb244f9e07debef99115a98383
Dave Glover <dglover@microsoft.com>
2019-09-06 19:04:32 +1000

    doco

--- Lab-setup/setup/0-setup.sh
+++ Lab-setup/setup/0-setup.sh
@@ -5,7 +5,7 @@ if [ $? -ne 0 ]; then
     exit $?
 fi
 
-~/PyLab/Lab-setup/setup/2-create-users.sh/1a-load-ftp.sh
+~/PyLab/Lab-setup/setup/1a-load-ftp.sh
 ~/PyLab/Lab-setup/setup/2-create-users.sh
 ~/PyLab/Lab-setup/setup/3-copy-lab.sh
 ~/PyLab/Lab-setup/setup/4-build-images.sh
