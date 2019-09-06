6e100c7913ab89fa9f8c2deaea689438af7f64fe
Dave Glover <dglover@microsoft.com>
2019-09-06 16:05:17 +1000

    doco

--- Lab-setup/setup/3-copy-lab.sh
+++ Lab-setup/setup/3-copy-lab.sh
@@ -6,8 +6,12 @@ git clone --depth 1 https://github.com/gloveboxes/PyCon-Hands-on-Lab.git ~/githu
 for i in {01..25}
 do
     echo "Set up lab content for user dev$i"
+    
     sudo rm -r -f /home/dev$i/PyLab
+    sudo mkdir -p /home/dev$i/PyLab
+
     sudo cp -r ~/github/Lab1-ssh-debug /home/dev$i/PyLab
     sudo cp -r ~/github/Lab2-docker-debug /home/dev$i/PyLab
+
     sudo chown -R dev$i:dev$i /home/dev$i
 done
